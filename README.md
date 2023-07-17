008f940bccc616856ecf1508f1434ec8ff8f5456
RomanOdessa75 <romeowrc@ukr.net>
2023-07-18 00:30:41 +0300

    nodejs-hw-1-1st-try

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # goit-nodejs-hw
+
+# Display contact list
+
+[node index.js --action list](https://ibb.co/r4xRFkH)
+
+# Get contact by id
+
+[node index.js --action get --id 05olLMgyVQdWRwgKfg5J6](https://ibb.co/Ss66YGT)
+
+# Add contact
+
+[node index.js --action add --name Mango --email mango@gmail.com --phone 322-22-22](https://ibb.co/0frk33R)
+
+# Remove contact
+
+[node index.js --action remove --id qdggE76Jtbfd9eWJHrssH](https://ibb.co/LNYY0LZ)
--- contacts.js
+++ contacts.js
@@ -1,24 +1,48 @@
-const contactsPath = ;
+import fs from "fs/promises";
+import path from "path";
+import { nanoid } from "nanoid";
 
-function listContacts() {
-  // ...твой код. Возвращает массив контактов.
-}
+const contactsPath = path.resolve("db", "contacts.json");
 
-function getContactById(contactId) {
-  // ...твой код. Возвращает объект контакта с таким id. Возвращает null, если объект с таким id не найден.
-}
+export const listContacts = async () => {
+  const contacts = await fs.readFile(contactsPath);
+  return JSON.parse(contacts);
+};
 
-function removeContact(contactId) {
-  // ...твой код. Возвращает объект удаленного контакта. Возвращает null, если объект с таким id не найден.
-}
+export const getContactById = async (contactId) => {
+  const contacts = await listContacts();
+  const result = contacts.find(({ id }) => id === contactId);
+  return result || null;
+};
 
-function addContact(name, email, phone) {
-  // ...твой код. Возвращает объект добавленного контакта. 
-}
+export const removeContact = async (contactId) => {
+  const contacts = await listContacts();
+  const index = contacts.findIndex(({ id }) => id === contactId);
+  if (index === -1) {
+    return null;
+  }
+  // const result = contacts.splice(index, 1)[0];
+  const [result] = contacts.splice(index, 1);
+  await fs.writeFile(contactsPath, JSON.stringify(result, null, 2));
+  return result;
+};
 
-module.exports {
-	listContacts,
-	getContactById,
-	removeContact,
-	addContact,
-}
+export const addContact = async (name, email, phone) => {
+  const contacts = await listContacts();
+  const newContact = {
+    id: nanoid(),
+    name,
+    email,
+    phone,
+  };
+  contacts.push(newContact);
+  await fs.writeFile(contactsPath, JSON.stringify(contacts, null, 2));
+  return newContact;
+};
+
+export default {
+  listContacts,
+  getContactById,
+  removeContact,
+  addContact,
+};
